ef0541b589cbf45e894ed0f61d9d96efbd0dfaef
kmg7 <kmlgkcy.dev@gmail.com>
2024-03-18 22:35:17 +0300

    substituion fixed

--- w2/substitution.py
+++ w2/substitution.py
@@ -1,63 +1,62 @@
+import math
 import random
+from route import letters
 
 def main():
-    TUR_U= 'ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ'
-    TUR_L= 'abcçdefgğhıijklmnoöprsştuüvyz'
-    text= "Kemal Gökçay"
-
     text = input("Text: ")
-    print(f"To use random generated key leave it empty")
-    key = input("Custom Key: ")
-    if key == "":
-        key = shuffle(TUR_L)
+    if text == "":
+        text = "Kemal Gökçay"
 
-    print(f"Key:       {key}") 
+    text = letters(text)
+    key = 4
+    key_input = input("Input Key: ")
+    if key_input != "":
+        try:
+            key = int(key_input)
+        except:
+            print("Invalid key. Key must be an integer.")
+            exit(1)
+    
     print(f"Input:     {text}")
 
-    crypted = substitute(TUR_U, TUR_L, text, key)
+    crypted = substitute(text, key)
     print(f"Crypted:   {crypted}")
 
-    uncrypted = de_substitue(TUR_L, crypted, key)
+    uncrypted = de_substitue(crypted, key)
     print(f"Uncrypted: {uncrypted}")
 
 
-def substitute(alphabet_U: str,alphabet_L: str, text:str, key:str):
+def substitute(text:str, key:str):
     crypted = ""
-    lenL = len(alphabet_L)
+    col = key
+    row = math.ceil(len(text) / key)
+    if row * col > len(text):
+        text += "a" * (row * col - len(text))
 
-    if len(key) != lenL:
-        return "Key length does not match with alphabet length"
+    text_chunks = []
+    for i in range(0, len(text), col):
+        text_chunks.append(text[i:i+col])
 
-    for letter in text:
-        newLetter = "#"
-        if letter.isalpha():
-            if letter.isupper():
-                i = alphabet_U.index(letter)
-                newLetter = key[i]
-            else:
-                i = alphabet_L.index(letter)
-                newLetter = key[i]
-        else:
-            newLetter = ""
-        crypted += newLetter
+    for i in range(col):
+        for j in range(row):
+            crypted += text_chunks[j][i]
+
     return crypted
 
-def de_substitue(alphabet_L: str, text:str, key:str):
-    encrypted = ""
-    lenL = len(alphabet_L)
+def de_substitue(text:str, key:str):
+    uncrypted = ""
+    row = key
+    col = len(text) // key
 
-    if len(key) != lenL:
-        return "Key length does not match with alphabet length"
+    text_chunks = []
+    for i in range(0, len(text), col):
+        text_chunks.append(text[i:i+col])
 
-    for letter in text:
-        newLetter = "#"
-        if letter.isalpha():
-            i = key.index(letter)
-            newLetter = alphabet_L[i]
-        else:
-            newLetter = ""
-        encrypted += newLetter
-    return encrypted
+    for i in range(col):
+        for j in range(row):
+            uncrypted += text_chunks[j][i]
+
+    return uncrypted
 
 def shuffle(text: str):
     text = list(text)
